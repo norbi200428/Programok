b8a8b2241b725b579283224c5b8da6ba2a35f2b0
norbi200428 <91279315+norbi200428@users.noreply.github.com>
2021-10-21 07:08:04 +0300

    Add files via upload

---
+++ string kereses/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+    string s="";
+    string str="az eg kek a fak lombkoronaja zold";
+    size_t found=str.find(s);
+    cout <<found<<endl;
+
+    return 0;
+}
