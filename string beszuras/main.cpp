8c9f6192153d333122c300b5b0f3192e3fe75f41
norbi200428 <91279315+norbi200428@users.noreply.github.com>
2021-10-21 07:07:00 +0300

    Add files via upload

---
+++ string beszuras/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s="z";
+    string str="a kek eg";
+    str.insert(1,s);
+    cout << str;
+    return 0;
+}
